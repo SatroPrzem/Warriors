1513b55dc00fcbcce84dc642c23fa78dd58ae5ea
Korean Przemek <przemyslaw.satro@gmail.com>
2022-08-06 01:41:55 +0200

    fight in progress

--- routes/arena.ts
+++ routes/arena.ts
@@ -1,11 +1,40 @@
 import { Router } from "express";
+import { WarriorRecord } from "../records/warrior.record";
+import { ValidationError } from "../util/error";
+import { fight } from "../util/fight";
 
 export const arenaRouter = Router();
 
 arenaRouter
-  .get("/fight-form", (req, res) => {
-    res.render("arena/fight-form");
+  .get("/fight-form", async (req, res) => {
+    const warriors = await WarriorRecord.listAll();
+    // console.log(warriors);
+    res.render("arena/fight-form", {
+      warriors,
+    });
   })
-  .post("/", (req, res) => {
-    res.send("OH GODS! FIGHT");
+  .post("/fight", async (req, res) => {
+    const { warrior1: warrior1Id, warrior2: warrior2Id } = req.body;
+    // console.log(req.body);
+    if (warrior1Id === warrior2Id) {
+      throw new ValidationError("Musisz wybrać 2 róźnych wojowników");
+    }
+
+    const warrior1 = await WarriorRecord.getOne(warrior1Id);
+    const warrior2 = await WarriorRecord.getOne(warrior2Id);
+
+    if (!warrior1) {
+      throw new ValidationError("Przepraszamy nie znaleziono wojownika nr. 1");
+    }
+    if (!warrior2) {
+      throw new ValidationError("Przepraszamy nie znaleziono wojownika nr. 2");
+    }
+    const { log, winner } = fight(warrior2, warrior2);
+
+    winner.wins++;
+    console.log("kurwa winner", winner);
+    await winner.update();
+    res.render("arena/fight", {
+      log,
+    });
   });
